c3304429986f73fd18acd2699046890c3f020b99
munir sebikaaka <munirsebikaaka@gmail.com>
2024-07-12 07:37:41 +0300

    started building app

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+const p = document.querySelector(".par");
+const buttons = document.querySelector(".btn");
+const string = "sfgdgshbhgdbdvcbhbfgvsf";
+buttons.addEventListener("click", function () {
+  p.textContent = string;
+});
