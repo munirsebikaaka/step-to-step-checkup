8381400262f2b693a91ef74ff4fb7b59c45a70dc
munir sebikaaka <munirsebikaaka@gmail.com>
2024-07-15 14:43:33 +0300

    feat:all changes committed

--- script.js
+++ script.js
@@ -43,57 +43,35 @@ const socialPlatform = document.querySelector("social-platform");
 
 const btnRunQuote = document.querySelector(".btn-quote");
 
-// buttons.addEventListener("click", function () {
-//   if (firstName.value && lastName.value) {
-//     name1.textContent = `your first name is ${firstName.value}`;
-//     name2.textContent = `ur last name is ${lastName.value}`;
-//     try1.textContent = "option1";
-//     ageSexCell.classList.remove("hidden");
-//   } else {
-//     alert("fill inputs");
-//   }
-//   if (+birthDates.value && typeOfSEX) {
-//     birthYear.textContent = `u wea born in ${birthDates.value}`;
-//     typeOfSEX.textContent = `u a a ${typeOfSEX.value}`;
-//     try2.textContent = "option2";
-//     origin.classList.remove("hidden");
-//   }
-//   if (country.value && district.value) {
-//     yourCountry.textContent = `u a from ${country.value}`;
-//     region.textContent = `ur region is ${district.value}`;
-//     try3.textContent = "option3";
-//     quotesCell.classList.remove("hidden");
-//     about.classList.remove("hidden");
-//   }
-// });
-
 //LOGIC STRUCTURE OF QUOTES
-const string = ["come", "go", "who", "why"];
+const appQuotes = document.querySelectorAll(".quotes-content");
+const quotePal = document.querySelector(".quote");
 
-btnRunQuote.addEventListener("click", function () {
-  small = string[Math.trunc(Math.random() * string.length)];
-  document.querySelector(".quote").textContent = small;
-});
-
-// const platformsCell = document.querySelector(".platforms-cell");
-// platformsCell.addEventListener("click", function (e) {
-//   if (e.target.classList.contains("platform")) console.log(e.target);
-// });
-// const x = document.querySelectorAll(".platform");
-// x.forEach(
-//   (el) =>
-//     //   el.addEventListener("click", function () {
-//     console.log(el)
-//   //   })
-// );
+const string = [
+  appQuotes[0].textContent,
+  appQuotes[1].textContent,
+  appQuotes[2].textContent,
+  appQuotes[3].textContent,
+  appQuotes[4].textContent,
+  appQuotes[5].textContent,
+];
 
 const appSteps = document.querySelectorAll(".try-text");
 const btnRigth = document.querySelector(".controls-right");
+const btnleft = document.querySelector(".controls-left");
+function movingToTheLeftPosition() {
+  btnleft.addEventListener("click", function () {
+    ageSexCell.classList.toggle("hidden");
+    appSteps[1].style.backgroundColor = "red";
+    origin.classList.toggle("hidden");
+  });
+}
 appSteps[0].style.backgroundColor = "green";
 const movingToTheRightPosition = () => {
   if (firstName.value && lastName.value) {
     appSteps[1].style.backgroundColor = "green";
     ageSexCell.classList.remove("hidden");
+    movingToTheLeftPosition();
   } else {
     alert("fill inputs");
   }
@@ -108,3 +86,13 @@ const movingToTheRightPosition = () => {
   }
 };
 btnRigth.addEventListener("click", movingToTheRightPosition);
+
+btnRunQuote.addEventListener("click", function () {
+  small = string[Math.trunc(Math.random() * string.length)];
+  quotePal.textContent = small;
+});
+document.querySelector(".btn-quote2").addEventListener("click", function () {
+  appSteps[4].style.backgroundColor = "green";
+  document.querySelector(".selected").textContent = quotePal.textContent;
+  platforms.classList.remove("hidden");
+});
