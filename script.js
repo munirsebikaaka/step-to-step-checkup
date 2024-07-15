8bd647a39c0a7cc2bdfda36e6dac62600da08009
munir sebikaaka <munirsebikaaka@gmail.com>
2024-07-15 10:43:26 +0300

    feat:added the step cells

--- script.js
+++ script.js
@@ -43,37 +43,32 @@ const socialPlatform = document.querySelector("social-platform");
 
 const btnRunQuote = document.querySelector(".btn-quote");
 
-buttons.addEventListener("click", function () {
-  if (firstName.value && lastName.value) {
-    name1.textContent = `your first name is ${firstName.value}`;
-    name2.textContent = `ur last name is ${lastName.value}`;
-    try1.textContent = "option1";
-    ageSexCell.classList.remove("hidden");
-  } else {
-    alert("fill inputs");
-  }
-  if (+birthDates.value && typeOfSEX) {
-    birthYear.textContent = `u wea born in ${birthDates.value}`;
-    typeOfSEX.textContent = `u a a ${typeOfSEX.value}`;
-    try2.textContent = "option2";
-    origin.classList.remove("hidden");
-  }
-  if (country.value && district.value) {
-    yourCountry.textContent = `u a from ${country.value}`;
-    region.textContent = `ur region is ${district.value}`;
-    try3.textContent = "option3";
-    quotesCell.classList.remove("hidden");
-    about.classList.remove("hidden");
-  }
-});
+// buttons.addEventListener("click", function () {
+//   if (firstName.value && lastName.value) {
+//     name1.textContent = `your first name is ${firstName.value}`;
+//     name2.textContent = `ur last name is ${lastName.value}`;
+//     try1.textContent = "option1";
+//     ageSexCell.classList.remove("hidden");
+//   } else {
+//     alert("fill inputs");
+//   }
+//   if (+birthDates.value && typeOfSEX) {
+//     birthYear.textContent = `u wea born in ${birthDates.value}`;
+//     typeOfSEX.textContent = `u a a ${typeOfSEX.value}`;
+//     try2.textContent = "option2";
+//     origin.classList.remove("hidden");
+//   }
+//   if (country.value && district.value) {
+//     yourCountry.textContent = `u a from ${country.value}`;
+//     region.textContent = `ur region is ${district.value}`;
+//     try3.textContent = "option3";
+//     quotesCell.classList.remove("hidden");
+//     about.classList.remove("hidden");
+//   }
+// });
 
 //LOGIC STRUCTURE OF QUOTES
 const string = ["come", "go", "who", "why"];
-string.forEach((el) =>
-  el.addEventListener("click", function () {
-    console.log("cliclked");
-  })
-);
 
 btnRunQuote.addEventListener("click", function () {
   small = string[Math.trunc(Math.random() * string.length)];
@@ -90,4 +85,26 @@ btnRunQuote.addEventListener("click", function () {
 //     //   el.addEventListener("click", function () {
 //     console.log(el)
 //   //   })
-// );j
+// );
+
+const appSteps = document.querySelectorAll(".try-text");
+const btnRigth = document.querySelector(".controls-right");
+appSteps[0].style.backgroundColor = "green";
+const movingToTheRightPosition = () => {
+  if (firstName.value && lastName.value) {
+    appSteps[1].style.backgroundColor = "green";
+    ageSexCell.classList.remove("hidden");
+  } else {
+    alert("fill inputs");
+  }
+  if (+birthDates.value && typeOfSEX) {
+    appSteps[2].style.backgroundColor = "green";
+    origin.classList.remove("hidden");
+  }
+  if (country.value && district.value) {
+    appSteps[3].style.backgroundColor = "green";
+    quotesCell.classList.remove("hidden");
+    about.classList.remove("hidden");
+  }
+};
+btnRigth.addEventListener("click", movingToTheRightPosition);
